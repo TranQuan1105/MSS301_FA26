822b98a6653246a79e77349e118c9f406df68635
hongquan1105 <quantran11052005@gmail.com>
2026-09-14 19:02:06 +0700

    feat(service): add ProductService

--- slot 3/ShoppingOnline/product-service/product-service/src/main/java/com/fudn/product_service/dto/ProductRequest.java
+++ slot 3/ShoppingOnline/product-service/product-service/src/main/java/com/fudn/product_service/dto/ProductRequest.java
@@ -2,5 +2,5 @@ package com.fudn.product_service.dto;
 
 import java.math.BigDecimal;
 
-public record ProductRequest(String id, String name, String description, BigDecimal Price) {
+public record ProductRequest(String id, String name, String description, BigDecimal price) {
 }
