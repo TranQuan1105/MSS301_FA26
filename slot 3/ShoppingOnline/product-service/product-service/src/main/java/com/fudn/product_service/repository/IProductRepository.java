fbd4d780ba9019d7e4d1456df57d3f153916fcd3
hongquan1105 <quantran11052005@gmail.com>
2026-09-14 18:54:33 +0700

    feat(repo): add IProductRepository

--- slot 3/ShoppingOnline/product-service/product-service/src/main/java/com/fudn/product_service/repository/IProductRepository.java
+++ slot 3/ShoppingOnline/product-service/product-service/src/main/java/com/fudn/product_service/repository/IProductRepository.java
@@ -3,5 +3,5 @@ package com.fudn.product_service.repository;
 import com.fudn.product_service.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ProductRepository extends MongoRepository<Product,String> {
+public interface IProductRepository extends MongoRepository<Product,String> {
 }
